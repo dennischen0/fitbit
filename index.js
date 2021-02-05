50962190f042280c6db7a82b57ba9e6978ad902d
Dennis Chen <c.w.dennis@gmail.com>
2021-02-05 12:21:43 -0800

    Update index

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
