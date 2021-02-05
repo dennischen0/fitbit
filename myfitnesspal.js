2f8ea6d921c928378d35236c024f01c16ea05690
Dennis Chen <c.w.dennis@gmail.com>
2021-02-05 12:13:49 -0800

    Serve html via nodejs

--- myfitnesspal.js
+++ myfitnesspal.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function() {
-  const month = 05;
+  const month = 1;
   const minDate = 01;
-  const maxDate = 31
+  const maxDate = 24
 
   for ( i = minDate; i <= maxDate; i++ ) {
     let stringMonth = String(month).padStart(2, '0')
     let stringDate = String(i).padStart(2, '0')
     let stringMinDate = String(minDate).padStart(2, '0')
-    let url = `https://www.myfitnesspal.com/food/copy_meal?date=2020-${stringMonth}-${stringDate}&from_date=2020-04-01&from_meal=0&username=mailyfesux`
+    let url = `https://www.myfitnesspal.com/food/copy_meal?date=2021-${stringMonth}-${stringDate}&from_date=2020-04-01&from_meal=0&username=mailyfesux`
     $.get(url, function(data, status) {
       console.log(`Inserted data for ${stringMonth}/${stringDate}`)
     })
