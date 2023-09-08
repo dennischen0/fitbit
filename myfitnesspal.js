c92ea21f0195cb1b7ae44eb38931c4b64b2810c1
Dennis Chen <c.w.dennis@gmail.com>
2023-09-08 11:16:03 -0700

    Remove node modules and make app run on localhost:

--- myfitnesspal.js
+++ myfitnesspal.js
@@ -5,15 +5,14 @@ jQuery.noConflict();
 
 
 $(document).ready(function() {
-  const month = 1;
-  const minDate = 01;
-  const maxDate = 24;
-  const year = 2022;
+  const month = moment().format('MM');
+  const minDate = 1;
+  const maxDate = parseInt(moment().format('DD'));
+  const year = moment().format('YYYY');
 
   for ( i = minDate; i <= maxDate; i++ ) {
     let stringMonth = String(month).padStart(2, '0')
     let stringDate = String(i).padStart(2, '0')
-    let stringMinDate = String(minDate).padStart(2, '0')
     let url = `https://www.myfitnesspal.com/food/copy_meal?date=${year}-${stringMonth}-${stringDate}&from_date=2020-04-01&from_meal=0&username=mailyfesux`
     $.get(url, function(data, status) {
       console.log(`Inserted data for ${stringMonth}/${stringDate}`)
